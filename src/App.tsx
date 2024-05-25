e9224dbad16ec14e5a5d1d645699a79e43222a91
alex <balaclava351@gmail.com>
2024-05-25 16:34:25 +0900

    [ADD] - introduction

--- src/App.tsx
+++ src/App.tsx
@@ -25,12 +25,28 @@ function App() {
 
         <ContentRight>
           <Section>
+            <h3>Introduction</h3>
+            <IntroUL>
+              <li>Experienced Frontend React Developer with 5 years of expertise in creating, maintaining, and deploying
+                high-quality React applications
+              </li>
+              <li>Proficient in developing hybrid mobile apps using React and CapacitorJS</li>
+              <li>Successfully delivered several projects from conception to deployment, ensuring robust
+                performance and user-friendly interfaces.
+              </li>
+              <li>
+                I'm Passionate about frontend development, I dedicate my free time to learning and experimenting with new technologies and features, constantly striving to enhance my skills and stay current with industry trends.
+              </li>
+            </IntroUL>
+          </Section>
+          <Section>
             <h3>Work History</h3>
-            <WorkHistory />
+            <WorkHistory/>
           </Section>
           <Section>
             <h3>Personal Projects</h3>
-            <WorkHistoryItem id={1} title={{role: 'Developer', company: 'Tome of D2'}} date={'2018 ~ Present'} stack={[]} summary={[]} />
+            <WorkHistoryItem id={1} title={{role: 'Developer', company: 'Tome of D2'}} date={'2018 ~ Present'}
+                             stack={[]} summary={[]}/>
           </Section>
         </ContentRight>
 
@@ -71,3 +87,16 @@ const ContentRight = styled.div`
     container-name: content-right;
 `;
 
+const IntroUL = styled.ul`
+    padding: 0.4rem 1rem;
+    
+  > li {
+    line-height: 160%;
+    font-size: 1.1rem;
+    text-wrap: pretty;
+  }
+  
+  > li:not(:last-of-type) {
+    margin-bottom: 0.2rem;
+  }
+`
