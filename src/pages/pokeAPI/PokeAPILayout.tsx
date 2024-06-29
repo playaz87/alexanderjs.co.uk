e5941499426636b5ad3c083b7fcd9fff3a43cbd5
alex <balaclava351@gmail.com>
2024-06-29 18:34:52 +0900

    poke api progress

--- src/pages/pokeAPI/PokeAPILayout.tsx
+++ src/pages/pokeAPI/PokeAPILayout.tsx
@@ -1,20 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 import { useAppDispatch, useAppSelector } from '../../store/store';
 import { fetchNextPokemonPage } from '../../store/poke-api/pokeApiSlice';
 import { selectorPokeApiResults } from '../../store/poke-api/selectors';
 import { PokemonOverview } from './components/PokemonOverview';
+import { useInfiniteScroll } from '../../common/hooks/useInfiniteScroll';
 
 const PokeApiLayout = (): React.ReactElement => {
   const results = useAppSelector(selectorPokeApiResults);
   const dispatch = useAppDispatch();
+  const container = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    dispatch(fetchNextPokemonPage());
-  }, []);
+  useInfiniteScroll(container, () => dispatch(fetchNextPokemonPage()));
 
   return (
-    <Container>
+    <Container ref={container}>
       {results.map(r => (
         <PokemonOverview key={r.name} resource={r} />
       ))}
@@ -28,6 +28,8 @@ const Container = styled.div`
   width: 100%;
   min-height: 100vh;
   min-height: 100svh;
+  max-height: 100vh;
+  max-height: 100svh;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(10rem, 20vw));
   grid-auto-rows: min-content;
@@ -35,4 +37,6 @@ const Container = styled.div`
   gap: 2rem;
   column-gap: 3%;
   background-color: var(--bg-1);
+  padding: 2rem 1rem;
+  overflow: auto;
 `;
